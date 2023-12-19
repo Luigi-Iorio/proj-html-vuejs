6504efbfe11a941559ce509ae2663f001a41be7e
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-19 17:14:19 +0100

    feat: fontawesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,17 @@ import { createApp } from "vue";
 import "./assets/scss/general.scss";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faSquareMinus } from "@fortawesome/free-solid-svg-icons";
+import { faPhoneFlip } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faSquareMinus, faPhoneFlip);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
