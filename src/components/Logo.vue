e9f2d94f3bd601ecce78902a10d1d3f35ab5b523
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-19 17:19:46 +0100

    feat: logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      imgLogo: "/img/avada-drivers-logo-2x-300x58.png",
+      altLogo: "logo avada",
+    };
+  },
+};
+</script>
+
+<template>
+  <!-- logo -->
+  <div class="logo">
+    <img :src="imgLogo" :alt="altLogo" />
+  </div>
+  <!-- /logo -->
+</template>
+
+<style lang="scss" scoped>
+img {
+  height: 100%;
+  display: block;
+}
+</style>
