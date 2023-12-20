356fbcb3ca913c74c6ba55a289662e531383ca27
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-20 22:48:56 +0100

    feat: arrow in footer component

--- src/components/FooterBottom.vue
+++ src/components/FooterBottom.vue
@@ -13,6 +13,9 @@ export default {};
       </p>
       <div class="icon">i i i i</div>
     </div>
+    <div class="arrow">
+      <div class="icon">i</div>
+    </div>
   </div>
   <!-- bottom -->
 </template>
@@ -20,9 +23,21 @@ export default {};
 <style lang="scss" scoped>
 @use "../assets/scss/partials/variables.scss" as *;
 .bottom {
+  position: relative;
   padding: 70px 0;
   background-color: $black;
   color: $athens;
+
+  .arrow {
+    text-align: center;
+    position: absolute;
+    bottom: 0;
+    right: 40px;
+    height: 40px;
+    width: 50px;
+    padding: 10px;
+    background-color: $tundora;
+  }
 
   .cont {
     width: 1180px;
